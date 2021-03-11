d6abe6e5753b7f58f8c671650f22d954dbf53659
stas <sgovorov569@gmail.com>
2021-03-11 18:27:04 +0500

    size array 2 on 2

--- src/com/company/Academy.java
+++ src/com/company/Academy.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import java.util.Arrays;
-
 public class Academy {
     String findMaximumElementMatrix(int[][] arr) {
         int row = 0;
@@ -34,6 +32,26 @@ public class Academy {
             for (int j = 0; j < arr[i].length ; j++) {
                 array[i][j] = arr[i][j];
             }
+        }
+    }
+    public static void printCorners(int[][]array){
+        int[][]arr = new int[2][2];
+        int row = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            int count = array.length;
+            for (int j = 0; j < arr[i].length ; j++) {
+                arr[i][j] = array[row][array.length - count];
+                count  = count - 2;
+            }
+            row = arr.length;
+        }
+
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j < arr[i].length ; j++) {
+                array[i][j] = arr[i][j];
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
