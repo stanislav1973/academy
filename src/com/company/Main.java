3be828e77fc1b70eafa52137c6a9ad256dc15c47
stas <sgovorov569@gmail.com>
2021-04-28 17:29:12 +0500

    passed the second stage

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       System.out.print("--------\n");
-        for (int i = 0; i < 3; i++) {
-            System.out.println("|" + "      " + "|");
+        int count = 0;
+        System.out.print("Enter cells:");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char[] t = str.toCharArray() ;
+        System.out.print("---------\n");
+
+        for (int i = 0; i < 3 ; i++) {
+            System.out.print("| " + t[count++]);
+            System.out.print(" " + t[count++]);
+            System.out.print(" " + t[count++] + " |");
+            System.out.print("\n");
         }
-        System.out.print("--------\n");
+        System.out.print("---------");
+        }
     }
-    }
+
 
 
 
