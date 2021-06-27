6e5e3b489f3bb16cecdd7958a50339128ed3d86b
stas <sgovorov569@gmail.com>
2021-06-27 19:56:56 +0500

    add new methods in class Battleship

--- src/com/company/Battleship.java
+++ src/com/company/Battleship.java
@@ -10,4 +10,26 @@ class Battleship {
         }
         return arr;
     }
+    static void HorizontalMarkup(){
+        System.out.print("  1" + " 2" + " 3" + " 4" + " 5" + " 6" + " 7" + " 8" + " 9" + " 10\n");
+    }
+    static char[] verticalMarkup() {
+        char[]charArray = new char[10];
+        for (int i = 0; i < charArray.length; i++) {
+            charArray[i] = (char) (65 + i);
+        }
+        return charArray;
+    }
+    static void inputFieldByConsole(){
+        String[][] arr = Battleship.getArray();
+        char[] alphabet = Battleship.verticalMarkup();
+        Battleship.HorizontalMarkup();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(alphabet[i]);
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(" " + arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
